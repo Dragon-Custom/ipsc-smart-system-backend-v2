e651dbe0a5f2a8e7c99daad6273aca04020251cf
normalid <rluo2018@gmail.com>
2024-06-20 11:02:08 +0800

    idk why the fuck its always complaining cannot find module :(

--- src/app/user/user.controller.spec.ts
+++ src/app/user/user.controller.spec.ts
@@ -1,20 +1,61 @@
-import { Test, TestingModule } from "@nestjs/testing";
+import { Test } from "@nestjs/testing";
 import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
+import { User } from "src/entities";
 
-describe("UserController", () => {
-	let controller: UserController;
+describe("UsersController", () => {
+	let usersController: UserController;
+	let usersService: UserService;
 
 	beforeEach(async () => {
-		const module: TestingModule = await Test.createTestingModule({
+		const moduleRef = await Test.createTestingModule({
 			controllers: [UserController],
 			providers: [UserService],
 		}).compile();
 
-		controller = module.get<UserController>(UserController);
+		usersService = moduleRef.get<UserService>(UserService);
+		usersController = moduleRef.get<UserController>(UserController);
 	});
 
-	it("should be defined", () => {
-		expect(controller).toBeDefined();
+	describe("findAll", () => {
+		it("should return an array of users", async () => {
+			const result: User[] = [
+				{
+					id: 1,
+					nickname: "User 1",
+					email: "user1@example.com",
+					encryptedPassword:
+						usersService.encryptePassword("password1"),
+					createdAt: new Date(),
+					isActive: true,
+					isBanned: false,
+					adminOfTeam: null,
+					designedStages: null,
+					ownsTeam: null,
+					shooterProfile: null,
+					stuffOfMatches: null,
+				},
+				{
+					id: 2,
+					nickname: "User 2",
+					email: "user2@example.com",
+					encryptedPassword:
+						usersService.encryptePassword("password2"),
+					createdAt: new Date(),
+					isActive: true,
+					isBanned: false,
+					adminOfTeam: null,
+					designedStages: null,
+					ownsTeam: null,
+					shooterProfile: null,
+					stuffOfMatches: null,
+				},
+			];
+			jest.spyOn(usersService, "findAll").mockImplementation(() =>
+				Promise.resolve(result),
+			);
+
+			expect(await usersController.findAll()).toBe(result);
+		});
 	});
 });
