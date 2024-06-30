d61ecba62c87dde6c226778250d7c8d460e2757f
normalid <rluo2018@gmail.com>
2024-06-30 13:32:22 +0800

    global user soft delete implemented

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,46 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import config from "./config";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { CrudConfigService } from "@nestjsx/crud";
+
+CrudConfigService.load({
+	query: {
+		limit: 25,
+		cache: 2000,
+		alwaysPaginate: true,
+		maxLimit: 100,
+		softDelete: true,
+	},
+	params: {
+		id: {
+			field: "id",
+			type: "number",
+			primary: true,
+		},
+	},
+	routes: {
+		updateOneBase: {
+			allowParamsOverride: true,
+		},
+		deleteOneBase: {
+			returnDeleted: true,
+		},
+	},
+});
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	const openApiConfig = new DocumentBuilder()
+		.setTitle("Cats example")
+		.setDescription("The cats API description")
+		.setVersion("1.0")
+		.addTag("cats")
+		.build();
+	const document = SwaggerModule.createDocument(app, openApiConfig);
+	SwaggerModule.setup("api", app, document);
+
 	await app.listen(config.server.port);
 }
 bootstrap();
