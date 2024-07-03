a0fdc4b540f911871fe136f7a61ff68df8b1a326
normalid <rluo2018@gmail.com>
2024-07-03 16:14:45 +0800

    clean up codes

--- src/api/users/users.controller.ts
+++ src/api/users/users.controller.ts
@@ -1,6 +1,6 @@
 import { Controller } from "@nestjs/common";
 import { UsersService } from "./users.service";
-import { Crud, CrudController, CrudRequest } from "@nestjsx/crud";
+import { Crud, CrudController } from "@nestjsx/crud";
 import { User } from "src/entities";
 import { mixinCrudConfig } from "src/types/mixinGlobalCRUDConfig";
 import { CreateUserDto, UpdateUserDto, UserDto } from "./users.dto";
@@ -22,8 +22,4 @@ import { ApiTags } from "@nestjs/swagger";
 )
 export class UsersController implements CrudController<User> {
 	constructor(public service: UsersService) {}
-
-	createOneBase(req: CrudRequest, dto: User): Promise<User> {
-		return this.service.createOne(req, dto);
-	}
 }
