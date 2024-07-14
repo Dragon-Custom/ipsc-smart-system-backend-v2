bb25f5e34dbe556e1d757a6c2710874d22cdc089
normalid <rluo2018@gmail.com>
2024-07-14 19:37:43 +0800

    implement ddq reason guard

--- src/api/matches/dq-reasons/dq-reasons.controller.ts
+++ src/api/matches/dq-reasons/dq-reasons.controller.ts
@@ -2,14 +2,20 @@ import { Controller } from "@nestjs/common";
 import { DqReasonsService } from "./dq-reasons.service";
 import { Crud, CrudController } from "@nestjsx/crud";
 import { DQReason } from "src/entities";
-import { mixinCrudConfig } from "src/utils";
+import {
+	CreateRouteGroup,
+	mixinCrudConfig,
+	RouteOperationPreset,
+} from "src/utils";
 import {
 	CreateDqReasonDto,
 	DqReasonDto,
 	UpdateDqReasonDto,
 } from "./dq-reasons.dto";
 import { ApiTags } from "@nestjs/swagger";
+import { AuthGuard } from "src/api/auth/auth.guard";
 
+//TODO: needs to refactor to enable permission based access control
 @Controller()
 @ApiTags("DQ Reasons")
 @Crud(
@@ -22,6 +28,16 @@ import { ApiTags } from "@nestjs/swagger";
 			update: UpdateDqReasonDto,
 			replace: CreateDqReasonDto,
 		},
+		routes: CreateRouteGroup([
+			{
+				route: [
+					...RouteOperationPreset.C,
+					...RouteOperationPreset.U,
+					...RouteOperationPreset.D,
+				],
+				guard: [AuthGuard],
+			},
+		]),
 	}),
 )
 export class DqReasonsController implements CrudController<DQReason> {
